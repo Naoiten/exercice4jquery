b51d1e58d8cac7cbac7d5ec6b16f94c5ac82c1c8
Antoine <antoine.hotiin32@laposte.net>
2019-07-19 16:56:54 +0200

    non finis

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+                $('#valider').click(function(){
+                    $("#age").value;
+                });
+                });
+
+                function appuyer()
+                    {
+                    $(document).ready(function () {
+                            var essai = $("#age").val();
+                            alert(essai);
+
+                    });
+                }
